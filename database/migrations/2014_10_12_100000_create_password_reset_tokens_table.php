483ce0fb0cf31fb40c191039b946d450062a2e9b
LUIS BRYAN CUEVA PARADA <luisbrian.lbcp@gmail.com>
2025-04-21 17:58:27 -0400

    fix

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -12,11 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email');
+            $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
-        
-            $table->primary('email');
         });
     }
 
